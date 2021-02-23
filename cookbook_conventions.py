59680db435b2ad6738c8c4ec10a7be52c30e0943
RunnanLi <lirunnan90@gmail.com>
2021-02-23 23:24:53 +1100

    cookbook conventions added

---
+++ cookbook_conventions.py
@@ -0,0 +1,61 @@
+"""
+Title : cookbook_conventions.py
+
+Description: Cookbook of Python conventions for pythonic coding
+
+Author: Runnan Li
+
+**Revision History:**
+
++---------+------------+------------+-----------------------------------------------------+
+| Version |     Date   |   Author   |                  Change Description                 |
++---------+------------+------------+-----------------------------------------------------+
+|   0.1   | 23/02/2021 | Runnan Li  |                   Initial Version                   |
++---------+------------+------------+-----------------------------------------------------+
+"""
+
+__version__ = 0.1
+
+CONSTANT_EXAMPLE = 0x1FFF
+
+
+class ClassUseCamelCase:
+    def __init__(self, variable):
+        self.myself = variable
+        self.__double_leading_underscore = 'attribute_1'
+
+    def fcn_use_lowercase(self, fcn_var, timeout=1):
+        """
+        Doc string here
+        """
+        print(self.myself, fcn_var, timeout)
+        raise ShowCaseError
+
+    @staticmethod
+    def static_fcn():
+        print('This is static function')
+
+    def _single_leading_underscore(self):
+        print(self.__double_leading_underscore, 'weak internal use indicator')
+
+    @classmethod
+    def class_methods(cls, var1):
+        print(var1)
+        instance = cls('class methods showcase')
+        print(instance.myself)
+
+
+class ShowCaseError(Exception):
+    """Show case of raise custom error"""
+    pass
+
+
+if __name__ == "__main__":
+    show_instance = ClassUseCamelCase(123)
+
+    try:
+        show_instance.fcn_use_lowercase('var1')
+    except ShowCaseError:
+        print('show case error caught')
+
+    show_instance.class_methods('class methods example')
